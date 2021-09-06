59626542dbd041dc8da036242cfe84925e8a5ee9
zhangjunpu <395352228@qq.com>
2021-09-07 07:43:43 +0800

    第三章内容

---
+++ 计算机基础/程序员的自我修养——链接, 装载和库/TestCode/SimpleSection.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//Linux
+// gcc -c SimpleSection.c
+//windows
+// 
+
+int global_init_var = 84;
+int global_uninit_var;
+
+printf(const char* format, ...);
+
+void func1(int i) {
+	printf("%d\n", i);
+}
+
+int int main(int argc, char const *argv[])
+{
+	static int static_var = 85;
+	static int static_var2;
+
+	int a = 1;
+	int b;
+	func1(static_var + static_var2 + a + b);
+	return a;
+}
